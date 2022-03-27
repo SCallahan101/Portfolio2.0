9fbc533bc15cc5bbd4b753058e6340f0de5fab10
Shannon Callahan <Shannonc619@gmail.com>
2022-03-27 12:46:49 -0600

    Toggle and display successful enabled
    Did some modificiations to get the toggle work with intro boxes work with display.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 function toggleIntro() {
-    const mainIntro = document.getElementsByClassName("regularIntro");
-    const secondaryIntro = document.getElementsByClassName("devIntro");
+    const mainIntro = document.getElementById("regularIntro");
+    const secondaryIntro = document.getElementById("devIntro");
     const toggleButton = document.getElementById("introToggle");
     if(toggleButton.checked){
-        console.log('it works');
+        // console.log('it works');
         // alert('it works');
+        mainIntro.style.display = "none";
+        secondaryIntro.style.display = "block";
+    } else {
+        mainIntro.style.display = "block";
+        secondaryIntro.style.display = "none";
     }
 }
 toggleIntro();
